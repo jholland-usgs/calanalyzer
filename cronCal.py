6456e9bcfcad1a9f99d085809522126e93122b75
Adam Baker <ambaker@usgs.gov>
2015-10-09 13:52:10 -0600

    changed order
    changed the order of the checking, giving preference to the more recent
    dates

--- cronCal.py
+++ cronCal.py
@@ -23,21 +23,20 @@ def printOutput(output):
 	for line in output:
 		if 'cal found' in line:
 			print line
-	
 
-#first, check one year
-# yearOne = years[jday % (len(years) - 1)]
-# output = commands.getstatusoutput('python multical.py -b ' + str(yearOne) + ',001 -e ' + str(yearOne) + ',366')
-# printOutput(output)
-
-#second, check one month last year
-# month = jday % 15 * 25 + 1
-# output = commands.getstatusoutput('python multical.py -b ' + str(yearCur - 1) + ',' + str(month).zfill(3) + ' -e ' + str(yearCur - 1) + ',' + str(month + 24).zfill(3))
-# printOutput(output)
-
-#third, check in steps
+#first, check in steps
 steps = [7, 30, 60, 90, 180]
 for step in steps:
 	if jday > step:
 		output = commands.getstatusoutput('python multical.py -b ' + str(yearCur) + ',' + str(jday - step).zfill(3) + ' -e ' + str(yearCur) + ',' + str(jday - step).zfill(3))
 		printOutput(output)
+
+#second, check one month last year
+month = jday % 15 * 25 + 1
+output = commands.getstatusoutput('python multical.py -b ' + str(yearCur - 1) + ',' + str(month).zfill(3) + ' -e ' + str(yearCur - 1) + ',' + str(month + 24).zfill(3))
+printOutput(output)
+
+#third, check one year
+yearOne = years[jday % (len(years) - 1)]
+output = commands.getstatusoutput('python multical.py -b ' + str(yearOne) + ',001 -e ' + str(yearOne) + ',366')
+printOutput(output)
