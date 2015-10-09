86dd302cf5b48645491d7b4542d03cfa08c2e66b
Adam Baker <ambaker@usgs.gov>
2015-10-09 13:09:54 -0600

    Test 2nd part
    Testing second part

--- cronCal.py
+++ cronCal.py
@@ -19,11 +19,14 @@ years.sort()
 yearFirst = years[0]
 
 #first, check one year
-yearOne = years[jday % (len(years) - 1)]
-output = commands.getstatusoutput('python multical.py -b ' + str(yearOne) + ',001 -e ' + str(yearOne) + ',366')
-print output
+# yearOne = years[jday % (len(years) - 1)]
+# output = commands.getstatusoutput('python multical.py -b ' + str(yearOne) + ',001 -e ' + str(yearOne) + ',366')
+# print output[:]
 
 #second, check one month last year
+month = jday % 15 * 25 + 1
+output = commands.getstatusoutput('python multical.py -b ' + str(yearCur - 1) + ',' + str(month).zfill(3) + ' -e ' + str(yearCur - 1) + ',' + str(month + 24).zfill(3))
+print output[:]
 
 #third, check last full month
 
