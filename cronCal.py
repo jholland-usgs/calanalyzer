25487b4f6225670fa8dae9712a971a84d3839132
Adam Baker <ambaker@usgs.gov>
2015-10-14 11:07:31 -0600

    minor logging tweak
    minor logging tweak, now adds a newline character after datestamps

--- cronCal.py
+++ cronCal.py
@@ -26,7 +26,7 @@ def printOutput(output):
 	for line in output:
 		if 'cal found' in line:
 			fob.write(line + '\n')
-	fob.write(str(UTCDateTime.now()))
+	fob.write('Scan completed at ' + str(UTCDateTime.now()) + '\n')
 	fob.close()
 
 #first, check in steps
