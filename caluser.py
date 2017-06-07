188ebb7373fea6cfe55a685a3b1db6f73664c951
Adam Baker <ambaker@usgs.gov>
2017-06-07 15:04:42 -0600

    Formatting/whitespace fix

--- caluser.py
+++ caluser.py
@@ -1,6 +1,8 @@
+#Fill in the information for the database you wish to use
+
 def info():
     host_address = ''
     dbname = ''
     username = ''
     password = ''
-	return dbname, username, host_address, password
+    return dbname, username, host_address, password
